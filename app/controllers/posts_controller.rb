e35bc316b116f0a4810772d7d4501f5ee08ea41f
koki kanasaka <k.koki980701@gmail.com>
2021-05-14 17:51:54 +0900

    ツイート詳細機能の追加

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -11,7 +11,9 @@ class PostsController < ApplicationController
     Post.create(post_params)
     redirect_to new_post_path
   end
-
+  def show
+    @post = Post.find(params[:id])
+  end
   private
   def post_params
     params.require(:post).permit(:content)
