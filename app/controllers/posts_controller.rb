89f384d67bb742a6422ab992f087008f90100f26
koki kanasaka <k.koki980701@gmail.com>
2021-05-14 17:24:07 +0900

    データを全て取得する機能

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,11 +1,19 @@
 class PostsController < ApplicationController
   def index
+    @post = Post.all.order(id: "DESC")
   end
+
   def new
     @post = Post.new
   end
+
   def create
-    Post.create(content: params[:post][:content])
+    Post.create(post_params)
     redirect_to new_post_path
+  end
+
+  private
+  def post_params
+    params.require(:post).permit(:content)
   end
 end
