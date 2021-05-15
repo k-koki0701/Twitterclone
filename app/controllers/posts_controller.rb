e3152906fb71fa9cf795e66a3b2825f46b264b1f
koki kanasaka <k.koki980701@gmail.com>
2021-05-15 10:30:45 +0900

    エラーメッセージ出力

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,12 +8,18 @@ class PostsController < ApplicationController
   end
 
   def create
-    Post.create(post_params)
-    redirect_to new_post_path
+    @post = Post.new(post_params)
+    if @post.save
+      redirect_to posts_path, notice: "ツイートしました！"
+    else
+      render :new
+    end
   end
+
   def show
     @post = Post.find(params[:id])
   end
+
   private
   def post_params
     params.require(:post).permit(:content)
