c7d6711b75020bfc9adf8ff30e78d8e7440ef6bf
koki kanasaka <k.koki980701@gmail.com>
2021-05-16 15:29:53 +0900

    不要なコメント削除

--- db/schema.rb
+++ db/schema.rb
@@ -1,6 +1,4 @@
 ActiveRecord::Schema.define(version: 2021_05_14_071047) do
-
-  # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "posts", force: :cascade do |t|
